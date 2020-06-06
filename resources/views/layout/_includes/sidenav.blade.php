6c40a156e46913ef275ce2ba5481f8e93282a321
Eduardo Luis Borba <eduardoborba3@gmail.com>
2020-06-06 16:51:47 -0300

    Implementação de icones no Sidenav

--- resources/views/layout/_includes/sidenav.blade.php
+++ resources/views/layout/_includes/sidenav.blade.php
@@ -20,9 +20,10 @@
       <a href="#name"><span class="white-text name">@yield('perfil')</span></a>
       <a href="#email"><span class="white-text email">@yield('email')</span></a>
     </div></li>
+    <li><a href="#!"><i class="material-icons">home</i>Home</a></li>
     <li><a href="#!"><i class="material-icons">person</i>Perfil</a></li>
-    <li><a href="#!">Second Link</a></li>
+    <!-- <li><a href="#!"><i class="material-icons">menu_book</i>Matérias</a></li> -->
     <li><div class="divider"></div></li>
-    <li><a class="subheader">Subheader</a></li>
-    <li><a href="{{ route('login.sair') }}">@yield('sair')</a></li>
+    <li><a href="#!"><i class="material-icons">settings</i>Configuração</a></li>
+    <li><a href="{{ route('login.sair') }}"><i class="material-icons">exit_to_app</i>Sair</a></li>
   </ul>
