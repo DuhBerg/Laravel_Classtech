1dc2028f32faf668c3f23463f2f951a20928faa8
Eduardo Luis Borba <eduardoborba3@gmail.com>
2020-06-08 13:13:02 -0300

    Organização de código / Mudança foto default

--- resources/views/layout/_includes/sidenav.blade.php
+++ resources/views/layout/_includes/sidenav.blade.php
@@ -17,20 +17,16 @@
     <li><div class="user-view">
       <div class="background"></div>
         <a href="#user"><img class="circle" src="{{$user->foto_perfil}}"></a>
+        <a href="#name"><span class="white-text name">@yield('perfil')</span></a>
+        <a href="#email"><span class="white-text email">@yield('email')</span></a>
+      </div>
+    </li>
 
-      <a href="#name"><span class="white-text name">@yield('perfil')</span></a>
-      <a href="#email"><span class="white-text email">@yield('email')</span></a>
-    </div></li>
-      @if($user->nivel_acesso == 'professor')
-      <li><a href="{{ route('professor.index')}}"><i class="material-icons">home</i>Home</a></li>
-      @elseif($user->nivel_acesso == 'aluno')
-      <li><a href="{{ route('aluno.index')}}"><i class="material-icons">home</i>Home</a></li>
-      @elseif($user->nivel_acesso == 'admin')
-      <li><a href="{{ route('admin.index')}}"><i class="material-icons">home</i>Home</a></li>
-      @endif
-
-    <li><a href={{ route('perfil.index') }} "><i class="material-icons">person</i>Perfil</a></li>
-    <!-- <li><a href="#!"><i class="material-icons">menu_book</i>Matérias</a></li> -->
+    <li><a href="{{ route('site.index') }}"><i class="material-icons">home</i>Home</a></li>
+    <li><a href="{{ route('perfil.index') }}"><i class="material-icons">person</i>Perfil</a></li>
+    @if($user->nivel_acesso == 'aluno')
+    <li><a href="#!"><i class="material-icons">menu_book</i>Cursos</a></li>
+    @endif
     <li><div class="divider"></div></li>
     <li><a href="#!"><i class="material-icons">settings</i>Configuração</a></li>
     <li><a href="{{ route('login.sair') }}"><i class="material-icons">exit_to_app</i>Sair</a></li>
