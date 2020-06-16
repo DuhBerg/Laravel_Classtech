94069234f815fa7ca5ffbe9e1ec2c7acc481d858
Eduardo Luis Borba <eduardoborba3@gmail.com>
2020-06-16 20:09:35 -0300

    Deletando código desnecessário.

--- resources/views/admin/adminTela.blade.php
+++ resources/views/admin/adminTela.blade.php
@@ -45,8 +45,6 @@
 
     <h4 class="light">Criar professor</h4>
 
-
-
       <form style="padding-top:15px;" class="form-login" action="{{ route('admin.criar')}}" method="post">
 
         <div class="row">
@@ -67,8 +65,7 @@
           <input type="password" name="password" id="senha">
           <label for="senha">Senha</label>
         </div>
-
-
+        
 
         <input type="hidden" name="nivel_acesso" value="professor">
 
@@ -125,9 +122,6 @@
           <label for="data">Data nascimento</label>
         </div>
 
-
-
-
         <input type="hidden" name="nivel_acesso" value="aluno">
 
         <input type="hidden" name="foto" value="img/fotos-perfil/default.png">
@@ -149,57 +143,6 @@
 
 
 
-
-
-<div class="modal modal-fixed-footer" id="modal-criar">
-  <div class="modal-content">
-
-    <h4 class="light">Criar professor</h4>
-
-
-
-      <form style="padding-top:15px;" class="form-login" action="{{ route('admin.criar')}}" method="post">
-
-        <div class="row">
-
-        {{ csrf_field() }}
-
-        <h3> Criar Professor </h3>
-        <div class="input-field">
-          <input type="text" name="ra" id="ra">
-          <label for="ra">Ra</label>
-        </div>
-
-        <div class="input-field">
-          <input type="text" name="name" id="name">
-          <label for="name">Nome</label>
-        </div>
-
-        <div class="input-field">
-          <input type="text" name="password" id="senha">
-          <label for="senha">Senha / Data de nascimento</label>
-        </div>
-
-
-        <br>
-
-        <input type="hidden" name="nivel_acesso" value="aluno">
-
-        <input type="hidden" name="foto" value="img/fotos-perfil/default.png">
-
-
-
-    </div>
-
-  </div>
-
-  <div class="modal-footer">
-
-      <button class="waves-effect waves-light btn indigo lighten-2">Criar</button>
-      <a class="modal-action modal-close waves-effect waves-red btn-flat">Cancelar</a>
-
-  </div>
-</div>
 
 
 
