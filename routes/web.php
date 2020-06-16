c983f03f793daf57144dafbafea25cecfce322e8
Celso Antonio Bertolotto Junior <celsonyl@hotmail.com>
2020-06-15 22:16:41 -0300

    explodi o site galera

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,14 +12,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/',['as'=>'site.index','uses'=>'site\siteController@index']);
 Route::get('/index',['as'=>'site.index','uses'=>'site\siteController@index']);
 
 Route::get('/login',['as'=>'site.login','uses'=>'site\loginController@index']);
 Route::post('/login/entrar',['as'=>'site.login.entrar','uses'=>'site\loginController@entrar']);
 Route::get('/login/sair',['as'=>'login.sair','uses'=>'site\loginController@sair']);
 
-
+Route::get('/forgotPassword','Security\ForgotPassword@forgot');
+Route::post('/forgotPassword','Security\ForgotPassword@password');
 
 Route::get('/cadastro',['as'=>'site.cadastro','uses'=>'site\cadastroController@index']);
 Route::post('/cadastro/criar',['as'=>'site.cadastro.criar','uses'=>'site\cadastroController@criar']);
@@ -45,7 +45,6 @@ Route::post('/professor/turmas/criar',['as'=>'professor.turmas.criar','uses'=>'p
 Route::get('/admin',['as'=>'admin.index','uses'=>'admin\adminController@index']);
 Route::post('/admin/criar',['as'=>'admin.criar','uses'=>'admin\adminController@criar']);
 
-Route::get('/forgotPassword','Security\ForgotPassword@forgot');
-Route::post('/forgotPassword','Security\ForgotPassword@password');
+
 
 });
